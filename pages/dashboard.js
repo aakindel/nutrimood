6e58bb0670b651d558cf5d44eb3506d0ae351c28
Leah Copeland <leah.copeland@neofinancial.com>
2021-09-11 15:26:44 -0600

    front end dashboard

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -5,8 +5,34 @@ export default function Dashboard() {
   return (
       <>
       <Navbar/>
-      <div className="grid grid-cols-1 gap-4 items-start lg:flex lg:space-x-4">
-      <div className="mt-4">
+      <div className="bg-gray-50 pt-12 sm:pt-16">
+      <div className="mt-2 pb-8 bg-white sm:pb-16">
+        <div className="relative">
+          <div className="absolute inset-0 h-1/2 bg-gray-50" />
+          <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="max-w-4xl mx-auto">
+              <dl className="rounded-lg bg-white shadow-lg sm:grid sm:grid-cols-3">
+                <div className="flex flex-col border-b border-gray-100 p-6 text-center sm:border-0 sm:border-r">
+                  <dt className="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">Pepperoni</dt>
+                  <dd className="order-1 text-5xl font-extrabold text-indigo-600">Mood</dd>
+                </div>
+                <div className="flex flex-col border-t border-b border-gray-100 p-6 text-center sm:border-0 sm:border-l sm:border-r">
+                  <dt className="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">Delivery</dt>
+                  <dd className="order-1 text-5xl font-extrabold text-indigo-600">Food</dd>
+                </div>
+                <div className="flex flex-col border-t border-gray-100 p-6 text-center sm:border-0 sm:border-l">
+                  <dt className="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">Calories</dt>
+                  <dd className="order-1 text-5xl font-extrabold text-indigo-600">
+                  <button>Daily Entry</button></dd>
+                </div>
+              </dl>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+      <div className="rounded-lg bg-white shadow w-full 'mt-12 mb-12 mx-12 pr-20">
+       <div className='mt-12 mb-12 mx-12'>
         <Line
           data={{
             labels: ['Sunday', 'Monday', 'Tuesday', 'Wednesday','Thursday','Friday', 'Saturday'],
