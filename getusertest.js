a2dbbf0f9449445bf7639a3f8ba6f5ac25d1c911
Monica Uwalaka <muwalaka@ualberta.ca>
2021-09-12 12:13:50 -0600

    fixed data migrations

--- getusertest.js
+++ getusertest.js
@@ -3,7 +3,10 @@ let db;
 async function getUsers(){
     db = await setup()
     const users = await db.all(`SELECT * FROM users`);
+    const entries = await db.all(`SELECT * FROM entries`);
     console.log(users);
+    console.log(entries);
+
  
 }
 
