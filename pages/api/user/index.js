3c89c8b051d41a6ccd26c9996510c92871adefc3
Ayo Akindele <a.akindele01@gmail.com>
2021-09-11 23:50:42 -0600

    add unique username validation to Sign Up page

--- pages/api/user/index.js
+++ pages/api/user/index.js
@@ -1,20 +1,40 @@
 const User = require('../../../models/user');
 
+async function validateUniqueUsername(username) {
+  let errorMsg = null;
+  const usernameInDb = await User?.getUser(username);
+
+  if (usernameInDb) {
+    errorMsg = "Username already used"
+  }
+
+  return errorMsg;
+}
+
 export default async function userHandler(req, res) {
   
   const method = req.method;
-  const {username, first_name, last_name, password} = req.body;
   
   switch (method) {
     case 'GET':
       /* Gets all users from database */
-      const users = await User.getAllUsers();
+      const users = await User?.getAllUsers();
       res.status(200).json({ users: users });
-      break
+      break;
     case 'POST':
+      const {username, first_name, last_name, password} = req.body;
+
+      const errorMsg = await validateUniqueUsername(username);
+      if (errorMsg) {
+        res.status(400);
+        res.json({ errorMsg });
+        return;
+      }
+
       /* Todo: create a new user */
-      const user = await User.createUser(username, first_name, last_name, password);
-      res.status(200).json({ user: user});
+      const user = await User?.createUser(username, 
+        first_name, last_name, password);
+      res.status(201).json({ user: user});
       break;
     default:
       res.setHeader('Allow', [ 'GET', 'POST']);
