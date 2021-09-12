ee888a71458a31e67b6cd042673ba20f5585842e
Monica Uwalaka <muwalaka@ualberta.ca>
2021-09-11 20:55:01 -0600

    added user and entry api endpoints

--- getusertest.js
+++ getusertest.js
@@ -1,10 +1,16 @@
 const {setup} = require('./database')
 async function getUsers(){
-    
     const db = await setup()
     const users = await db.all(`SELECT * FROM users`);
-
     console.log(users);
+    // await db.run(`INSERT INTO users (username, first_name, last_name, password)
+    // VALUES ('testuser8',
+    //         'Test',
+    //         'User',
+    //         'password'
+    //         )`);
+    // const userss = await db.all(`SELECT * FROM users`);
+    // console.log(userss);
 }
 
 getUsers();
--- migrations/001-users.sql
+++ migrations/001-users.sql
@@ -3,13 +3,15 @@ CREATE TABLE users (
   username VARCHAR(25) PRIMARY KEY,
   first_name TEXT NOT NULL,
   last_name TEXT NOT NULL,
-  password TEXT NOT NULL
+  password TEXT NOT NULL,
+  id VARCHAR(36) NOT NULL
 );
 
-INSERT INTO users (username, first_name, last_name, password)
+INSERT INTO users (username, first_name, last_name, password, id)
 VALUES ('testuser',
         'Test',
         'User',
-        'password'
+        'password',
+        'djnfidnrigndbi1e82'
         );
        
