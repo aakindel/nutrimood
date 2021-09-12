568c1825a0d9d8d4dd03056e061b4c55fc6ea528
Ayo Akindele <a.akindele01@gmail.com>
2021-09-12 12:14:31 -0600

    create UserContext

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
+import { useMemo, useState } from 'react'
 import '../styles/globals.css'
+import { UserContext } from '../UserContext'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [loggedInUser, setLoggedInUser] = useState({})
+  const providerValue = useMemo(() => {
+    (loggedInUser, setLoggedInUser)}, 
+    [loggedInUser, setLoggedInUser]
+  )
+
+  return (
+    <UserContext.Provider value={providerValue}>
+      <Component {...pageProps} />
+    </UserContext.Provider>
+  )
 }
 
 export default MyApp
